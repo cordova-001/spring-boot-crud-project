c828c659c2a9c2061a5b5f05db723821002afea2
cordova-001 <aabiola610@gmail.com>
2023-04-29 22:54:51 +0100

    Spring boot crud

---
+++ src/test/java/com/ahmad/firstcrudapp/FirstCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmad.firstcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
