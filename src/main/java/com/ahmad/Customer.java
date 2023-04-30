d703c5579b87c1f24c62e2b253329fff813b5459
cordova-001 <aabiola610@gmail.com>
2023-04-30 14:22:34 +0100

    Spring boot crud

---
+++ src/main/java/com/ahmad/Customer.java
@@ -0,0 +1,106 @@
+package com.ahmad;
+
+import java.util.Objects;
+
+public class Customer {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public Customer(Integer id,
+                    String firstName,
+                    String lastName,
+                    String email,
+                    String phone,
+                    String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public Customer() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    private String address;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(firstName, customer.firstName) && Objects.equals(lastName, customer.lastName) && Objects.equals(email, customer.email) && Objects.equals(phone, customer.phone) && Objects.equals(address, customer.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, phone, address);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/ahmad/Main.java
+++ src/main/java/com/ahmad/Main.java
@@ -1,16 +1,22 @@
+package com.ahmad;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//To make this a spring boot application we add @Spring-boot-application annotation to this
 @SpringBootApplication
 @RestController
+//@EnableSwagger2
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/greet")
-    public String greet(){
-        return "Hello World";
+
+    @GetMapping("/")
+    public  String greet(){
+        return "Hello";
     }
 }
